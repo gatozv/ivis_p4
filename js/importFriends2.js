0e096eb40e7435e275b529256a2142d32a75c2fe
ansierras <ansierras@gmail.com>
2015-03-11 16:15:28 +0100

    communities
    fixed some random problems with community detection stuff

--- js/importFriends2.js
+++ js/importFriends2.js
@@ -2,22 +2,33 @@ function importFriends2 () {
 	var data = parseXML("./xslt/allDataDiana.xml","./xslt/GoodReadsTrafo.xslt");
 	var friends = eval('(' + data + ')');
 	//console.log(friends);
+	var exists = false;
 	for (var i in friends) {
 		//var xmlFileURI = "./xslt/compare_"+friends[i].id+".xml";
 		//var friendData = parseXML(xmlFileURI,"./xslt/FriendCompareBooksTrafo.xslt");
-		
+		exists = true;
 		var xmlFileURI = "./xslt/"+friends[i].id+".xml";
-		var friendData = parseXML(xmlFileURI,"./xslt/FriendBooksTrafo.xslt");
+		try{
+			var friendData = parseXML(xmlFileURI,"./xslt/FriendBooksTrafo.xslt");
+		}catch(e){
+			//console.log(e);
+			exists = false;
+		}
 		
-		//console.log(friendData);
-		var friendInfo = eval('(' + friendData + ')');
-		
-		friends[i]["books"] = friendInfo
+		if (exists) {
+			//console.log(friendData);
+			var friendInfo = eval('(' + friendData + ')');
+			
+			friends[i]["books"] = friendInfo
 
-		xmlFileURI = "./xslt/compare_"+friends[i].id+".xml";
-		friendData = parseXML(xmlFileURI,"./xslt/FriendCompareBooksTrafo.xslt");
-		friendInfo = eval('(' + friendData + ')');
-		friends[i]["common_books"] = friendInfo;
+			xmlFileURI = "./xslt/compare_"+friends[i].id+".xml";
+			friendData = parseXML(xmlFileURI,"./xslt/FriendCompareBooksTrafo.xslt");
+			friendInfo = eval('(' + friendData + ')');
+			friends[i]["common_books"] = friendInfo;
+		}else{
+			friends[i]["books"] = [];
+			friends[i]["common_books"] = [];
+		};
 	/*	for (var ii in friendInfo) {
 			$.extend(friends[i], friendInfo[ii]);
 		}*/
