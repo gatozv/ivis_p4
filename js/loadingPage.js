b5fb7c429861460611878c1ede782b54f0aec818
Tomáš Gatial <tomas@sstatzz.com>
2015-03-12 21:36:32 +0100

    login animation added

--- js/loadingPage.js
+++ js/loadingPage.js
@@ -1,32 +1,9 @@
-var svgWidth = 600;
-var svgHeight = 50;
-var svgContainer = d3.select("#loadingScreen") //create container
-                    .append("svg")
-                    .attr("width", svgWidth)
-                    .attr("height", svgHeight);
-svgContainer.append("rect")
-            .attr("transform", "translate(" + svgWidth / 2 + "," + svgHeight / 2 + ")")
-            .attr("x", -svgWidth / 2)
-            .attr("y", -svgHeight / 2)
-            .attr("width", svgWidth)
-            .attr("height", svgHeight)
-            .style("fill", "white")
-            .style("stroke-width", 0.5)
-            .style("stroke", "#555555");
-var progress = svgContainer.append("rect")
-                            .attr("transform", "translate(" + svgWidth / 2 + "," + svgHeight / 2 + ")")
-                            .attr("x", -svgWidth / 2)
-                            .attr("y", -svgHeight / 2)
-                            .attr("width", 0)
-                            .attr("height", svgHeight)
-                            .style("fill", "blue");
-//.style("stroke-width",0.5)
-//.style("stroke","#555555")
+
 goodreadsDataFetches.statusUpdate = function(percentage, state) {
     console.log("percentage " + percentage + " state " + state)
-    var newwidht = (percentage / 100 * svgWidth);
-    progress.attr("width", newwidht);
-    document.getElementById("status").innerHTML = "state: " + state + " percentage: " + percentage + "%"
+    var newwidht = 1678-(percentage/100*1678);
+	d3.select(".path").transition().duration(1000).style("stroke-dashoffset", newwidht)		
+	document.getElementById("status").innerHTML = "Creating the big bang. " +percentage +"% of the universe ready"
     if(state=="done"){
         console.log("i'm here");
 
@@ -46,11 +23,6 @@ goodreadsDataFetches.finished = function(data) {
     console.log("allfriends");
     console.log(allFriends);
     drawStars(allFriends);
-    /*
-                allFriends.forEach(function(d,i){
-                    console.log("id "+ i +" commonwithme " + d.commonBooks + " community "+d.community+ " totalbooks "+d.totalBooks )
-                
-                })*/
 }
 var isUsingTestingDataset = getParameterByName("testingData") == "1"
 if (isUsingTestingDataset) {
