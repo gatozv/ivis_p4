801bc9526048d8985b205ce1103aa437312c656e
Tomáš Gatial <tomas@sstatzz.com>
2015-03-23 12:02:37 +0100

    cookies experiment 2

--- index.php
+++ index.php
@@ -45,6 +45,18 @@
 
 		setcookie("access_tokens", $access_token);
 
+		$cookie_name = "user";
+		$cookie_value = "Alex Porter";
+		setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+
+
+		if(!isset($_COOKIE[$cookie_name])) {
+		    echo "Cookie named '" . $cookie_name . "' is not set!";
+		} else {
+		    echo "Cookie '" . $cookie_name . "' is set!<br>";
+		    echo "Value is: " . $_COOKIE[$cookie_name];
+		}
+		
 		/*	echo '<script type="text/javascript">
 	           window.location = "skylikes.html"
 	      </script>';*/
