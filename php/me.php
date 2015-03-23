d20191bc34115876a721704cb9eff05d64ac3640
Tomáš Gatial <tomas@sstatzz.com>
2015-03-23 10:53:02 +0100

    test access token 2

--- php/me.php
+++ php/me.php
@@ -8,7 +8,10 @@ session_start();
 $obj = new GoodreadsApi(iSAfjzMo6QHWifwOfCQvQ, XRCIVEsXshWdT2NwKGqHoQ6e8qzC1YclV45Zgp2GADg, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
 	$content = $obj->doGet('http://www.goodreads.com/api/auth_user');
-	print_r("<!-- access token: ".$access_token['oauth_token']."-->");
+
+	$tok = $access_token['oauth_token']
+
+	echo "<!-- access token: $tok -->");
 	print_r($content);
 	unset ($obj);
 
